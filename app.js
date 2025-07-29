47ca4d30af883e7c1b654ebc9340c2febfb235a3
Muhammad Nafees <165241321+Mnafees76@users.noreply.github.com>
2025-07-29 23:33:10 +0500

    Create app.js
    Create app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+var furits = ["Apple","Mango","Banana",]
+console.log(furits)
+
+
+var furits = ["Apple","Mango","Banana",]
+furits.pop();
+
+
+var furits = ["Apple","Mango","Banana",]
+furits.remove();
+
+
+// -------end---------------
+
+
+
+
+var a = 23 ;
+var b = 10 ;
+console.log(a+b)
+
+
+
+
+
+var a = 10 ;
+var b = 4 ;
+console.log(a-b)
+
+
+
+var a = 10 ;
+var b = 4 ;
+console.log(a*b)
+
+
+
+var a = 3 ;
+var b = 7 ;
+console.log(a/b)
+
+
+
